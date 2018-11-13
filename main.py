e5da2c686897be51442e8ca1093de4802e9b0f04
Ciara Mae Gotis <crgotis@up.edu.ph>
2018-11-13 21:29:34 +0800

    initial send method

--- main.py
+++ main.py
@@ -20,12 +20,15 @@ def createLobby(sock):
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect the socket to the port where the server is listening
 server_address = ("202.92.144.45", 80)
-print("connecting to %s port %s" % server_address, file=sys.stderr)
+#print("connecting to %s port %s" % server_address, file=sys.stderr)
 sock.connect(server_address)
 
 packet = TcpPacket()
 connection = packet.ConnectPacket()
 connection.type = packet.CONNECT
+
+chat_packet = packet.ConnectPacket()
+chat_packet.type = packet.CONNECT
 
 
 choice = input("Create New Lobby? Y/N: ")
@@ -40,3 +43,32 @@ if choice == 'Y':
     lobbyDetails.ParseFromString(room)
     lobbyID = lobbyDetails.lobby_id
     print("lobbyID: {}".format(lobbyID))
+else:
+	lobbyID = input("Enter lobby ID: ")
+
+
+username = input("Enter username: ")
+
+#connects the new user
+packet.type = packet.CONNECT
+chat_packet.player.name = username
+chat_packet.lobby_id = lobbyID
+
+sock.send(chat_packet.SerializeToString())
+
+chat_send = packet.ChatPacket()
+chat_send.type = packet.CHAT
+chat_send.player.name = username
+chat_send.lobby_id = lobbyID
+
+while (True):
+	message = input("Message: ")
+	try:
+		chat_send.message = message
+		sock.send(chat_send.SerializeToString())
+		# receive = sock.recv(2048)
+		# finalMessage = chat_send.ParseFromString(receive)
+		# print("{}".format(finalMessage))
+	except OSError:
+		print("Error")
+		break
