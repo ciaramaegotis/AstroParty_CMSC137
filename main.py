cf6d068eee2104f2e3fd2ebf3e3aac9a851675e0
DavidBenavidez <ddbenavidez2@up.edu.ph>
2018-11-15 13:17:23 +0800

    Stash

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 #! /usr/bin/python
 
-from proto.tcp_packet_pb2 import TcpPacket
 import socket
 import sys
-import select
-import pickle
+
+from proto.tcp_packet_pb2 import TcpPacket
+
+from threading import Thread
+
 
 syntax = "proto2"
 
 
-def createLobby(sock):
-    lobbyDetails = TcpPacket()
-    lobbyDetails.type = 1
-    lobbyDetails.max_players = 3
-    sock.send(bytes())
-
+# def createLobby(sock):
+# def createMessage(data):
+# def listPlayers()
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect the socket to the port where the server is listening
 server_address = ("202.92.144.45", 80)
-#print("connecting to %s port %s" % server_address, file=sys.stderr)
+# print("connecting to %s port %s" % server_address, file=sys.stderr)
 sock.connect(server_address)
 
 packet = TcpPacket()
@@ -44,31 +43,47 @@ if choice == 'Y':
     lobbyID = lobbyDetails.lobby_id
     print("lobbyID: {}".format(lobbyID))
 else:
-	lobbyID = input("Enter lobby ID: ")
+    lobbyID = input("Enter lobby ID: ")
 
 
 username = input("Enter username: ")
 
-#connects the new user
+# Instantiate CONNECT Packet
 packet.type = packet.CONNECT
 chat_packet.player.name = username
 chat_packet.lobby_id = lobbyID
 
 sock.send(chat_packet.SerializeToString())
 
+# Instantiate ChatPacket
 chat_send = packet.ChatPacket()
 chat_send.type = packet.CHAT
 chat_send.player.name = username
 chat_send.lobby_id = lobbyID
 
+
+def parseData(data):
+    packet.ParseFromString(data)
+
+    if(packet.type == packet.CONNECT):
+        chat_send.ParseFromString(data)
+        print("%s gas joined the chat room" % chat_send.player.name)
+    elif packet.type == packet.CHAT:
+        chat_send.ParseFromString(data)
+        print('[{}] {}'.format(chat_send.player.name, chat_send.message))
+    else:
+        print('Packet received')
+
+        # Listen for other chats
+listening_thread = Thread(target=sock.recv, args=[])
 while (True):
-	message = input("Message: ")
-	try:
-		chat_send.message = message
-		sock.send(chat_send.SerializeToString())
-		# receive = sock.recv(2048)
-		# finalMessage = chat_send.ParseFromString(receive)
-		# print("{}".format(finalMessage))
-	except OSError:
-		print("Error")
-		break
+    message = input("Message: ")
+    try:
+        chat_send.message = message
+        sock.send(chat_send.SerializeToString())
+        # receive = sock.recv(2048)
+        # finalMessage = chat_send.ParseFromString(receive)
+        # print("{}".format(finalMessage))
+    except OSError:
+        print("Error")
+        break
