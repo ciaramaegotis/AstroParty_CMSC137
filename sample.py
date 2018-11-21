e8ad2d64957cedbcda3cf9002a2258b09cfe1599
root <crgotis@up.edu.ph>
2018-11-21 17:19:13 +0800

    text wrap

--- sample.py
+++ sample.py
@@ -1,27 +1,13 @@
-# import pygame
-
-# (width, height) = (850, 450)
-# bg = pygame.image.load("background.jpeg")
-# chat_panel = pygame.image.load("obstacle.png")
-# chat_panel = pygame.transform.scale(chat_panel, (250, 430))
-# screen = pygame.display.set_mode((width, height))
-# screen.blit(bg, (0, 0))
-# screen.blit(chat_panel, (10, 10))
-# pygame.display.flip()
-# while True:
-# 	pass
-
 import pygame as pg
-
 
 def main():
 	bg = pg.image.load("background.jpeg")
 	chat_panel = pg.image.load("obstacle.png")
 	chat_panel = pg.transform.scale(chat_panel, (250, 430))
 	screen = pg.display.set_mode((850, 450))
-	font = pg.font.Font(None, 32)
+	font = pg.font.Font(None, 25)
 	clock = pg.time.Clock()
-	input_box = pg.Rect(20, 380, 80, 50)
+	input_box = pg.Rect(20, 380, 225, 50)
 	color_inactive = pg.Color('lightskyblue3')
 	color_active = pg.Color('dodgerblue2')
 	color = color_inactive
@@ -50,13 +36,19 @@ def main():
 					else:
 						text += event.unicode
 
-		screen.fill((30, 30, 30))
+		screen.fill((10, 10, 10))
 		txt_surface = font.render(text, True, color)
-		width = max(200, txt_surface.get_width()+10)
-		input_box.w = width
 		screen.blit(bg, (0, 0))
 		screen.blit(chat_panel, (10, 10))
-		screen.blit(txt_surface, (20, 380))
+		if (txt_surface.get_width() > 155):
+			newResult = [text[i:i+20] for i in range(0, len(text), 20)]
+			start_col = 380
+			for j in newResult:
+				print("here")
+				screen.blit(font.render(j, True, color), (23, start_col))
+				start_col = start_col + 17
+		else:
+			screen.blit(txt_surface, (23, 380))
 		pg.draw.rect(screen, color, input_box, 2)
 		pg.display.flip()
 		clock.tick(30)
