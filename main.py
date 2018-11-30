7dc2a5be89a674e27791fedc7c457d2d68117a1e
root <crgotis@up.edu.ph>
2018-11-30 22:13:57 +0800

    chat box

--- main.py
+++ main.py
@@ -12,18 +12,41 @@ syntax = "proto2"
 screen = pg.display.set_mode((850, 450))
 bg = pg.image.load("background.jpeg")
 clock = pg.time.Clock()
+current_num_of_players = 0
 
 def receiveData(callback):
     while True:
         data = sock.recv(2048)
         callback(data)
 
+def paintNewPlayer():
+    print(current_num_of_players)
+    print("HAHAHAHAHAHA :(")
+    if (current_num_of_players == 1):
+        pg.draw.rect(screen,(0,0,0),(700,200,50,50));
+    elif (current_num_of_players == 2):
+        pg.draw.rect(screen,(0,0,0),(700,200,50,50));
+        pg.draw.rect(screen,(0,0,0),(700,250,50,50));
+    elif (current_num_of_players == 3):
+        pg.draw.rect(screen,(0,0,0),(700,200,50,50));
+        pg.draw.rect(screen,(0,0,0),(700,250,50,50));
+        pg.draw.rect(screen,(0,0,0),(700,300,50,50));
+    else:
+        pg.draw.rect(screen,(0,0,0),(700,200,50,50));
+        pg.draw.rect(screen,(0,0,0),(700,250,50,50));
+        pg.draw.rect(screen,(0,0,0),(700,300,50,50));
+        pg.draw.rect(screen,(0,0,0),(700,350,50,50));
+    pg.display.flip()
+    clock.tick(30)
 
 def evaluateData(data):
+    global current_num_of_players
     packet.ParseFromString(data)
     if (packet.type == packet.CONNECT):
         chat_packet.ParseFromString(data)
         print("\n{} joined the chat.".format(chat_packet.player.name))
+        current_num_of_players += 1
+        paintNewPlayer()
     elif (packet.type == packet.CHAT):
         chat_send.ParseFromString(data)
         print("\n{}: {}".format(chat_send.player.name, chat_send.message))
@@ -229,6 +252,8 @@ def createNewLobby():
                     except:
                         print("\nError in creating/entering a lobby~")
                         quit()
+                    global current_num_of_players
+                    current_num_of_players = len(listPlayers())
                     return lobbyID, username, 0
                 elif quit_button.collidepoint(event.pos):
                     print("quit button was pressed!")
@@ -270,11 +295,16 @@ def listPlayers():
 
 
 def startChat():
+    paintNewPlayer()
     screen.blit(bg, (0, 0))
     waitOtherPlayers = pg.image.load("waitingPlayers.png")
     waitOtherPlayers = pg.transform.scale(waitOtherPlayers, (700, 300))
     chat_panel = pg.image.load("chat_panel.png");
     chat_panel = pg.transform.scale(chat_panel, (250, 440))
+    start_button = pg.image.load("backbut.png")
+    start_button = pg.transform.scale(start_button, (400, 250))
+    start_button = pg.transform.flip(start_button, True, False);
+
     player_1 = pg.image.load("chat_panel.png")
     player_1 = pg.transform.scale(player_1, (150, 150));
     player_2 = pg.image.load("chat_panel.png")
@@ -283,13 +313,18 @@ def startChat():
     player_3 = pg.transform.scale(player_3, (150, 150));
     player_4 = pg.image.load("chat_panel.png")
     player_4 = pg.transform.scale(player_4, (150, 150));
+
     
     screen.blit(waitOtherPlayers, (200, -100))
     screen.blit(chat_panel, (5, 5))
-    screen.blit(player_1, (350, 90))
-    screen.blit(player_2, (350, 270))
-    screen.blit(player_3, (600, 90))
-    screen.blit(player_4, (600, 270))
+    screen.blit(player_1, (380, 90))
+    screen.blit(player_2, (380, 270))
+    screen.blit(player_3, (570, 90))
+    screen.blit(player_4, (570, 270))
+    start_button_detector = pg.draw.rect(screen,(0,0,0),(760,230,50,50))
+    screen.blit(start_button, (600, 120))
+
+
     pg.display.flip()
     clock.tick(30)
 
@@ -298,25 +333,50 @@ def startChat():
     receiving_thread = Thread(target=receiveData, args=[evaluateData])
     receiving_thread.start()
 
+    font = pg.font.Font(None, 32)
+    input_box = pg.Rect(320, 250, 140, 32)
+    message = ""
+    color = pg.Color("orange")
+    #listen to the start button and to the chatbox and to the incoming players
     while (True):
-        try:
-            message = input("")
-            if(messag == "dc()"):
-                chat_disconnect.player.name = username
-                sock.send(chat_disconnect.SerializeToString())
-                isBreak = True
-                quit()
-            elif(message == "list()"):
-                playersList = listPlayers()
-                for player in playersList:
-                    print(player)
-                print(">> ", end='')
-            else:
-                chat_send.message = message
-                sock.send(chat_send.SerializeToString())
-        except OSError:
-            print("\nError")
-            break
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                break
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if start_button_detector.collidepoint(event.pos):
+                    print("START GAME!")
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_RETURN:
+                    #send the message
+                    try:
+                        if(message == "dc()"):
+                            chat_disconnect.player.name = username
+                            sock.send(chat_disconnect.SerializeToString())
+                            isBreak = True
+                            quit()
+                        elif(message == "list()"):
+                            playersList = listPlayers()
+                            for player in playersList:
+                                print(player)
+                            print(">> ", end='')
+                        else:
+                            chat_send.message = message
+                            sock.send(chat_send.SerializeToString())
+                    except OSError:
+                        print("\nError")
+                    done = True
+                    message = ''
+                elif event.key == pg.K_BACKSPACE:
+                    message = message[:-1]
+                else:
+                    message += event.unicode
+        txt_surface = font.render(message, True, color)
+        width = max(200, txt_surface.get_width()+10)
+        input_box.w = width
+        screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
+        pg.draw.rect(screen, color, input_box, 2)
+        pg.display.flip()
+        clock.tick(30)
 
 
 # Create a TCP/IP socket and connect the socket to the port where the server is listening
