8940827a1e1c4f3c50722d38ad2dde8c8c1607e3
root <crgotis@up.edu.ph>
2018-12-08 16:10:43 +0800

    sep class for game objs

--- src/game/displays/gameplay.py
+++ src/game/displays/gameplay.py
@@ -10,145 +10,11 @@ sys.path.append("..")
 from utils.button import Button
 from utils.variables import *
 from utils.images import *
+from .wall import Wall
+from .player import Player
+from .bullet import Bullet
 
 all_sprite_list = pg.sprite.Group()
-
-class Player(pg.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        #player
-        self.image = rocket_ship
-        self.image = pg.transform.scale(self.image, (100, 50))
-        self.rect = self.image.get_rect()
-        self.rect.y = y
-        self.rect.x = x
-
-        self.direction = 1
-        self.walls = None
-
-    def rotate(self):
-        print("ROTATE!")
-        self.image = pg.transform.rotate(self.image, 270)
-        self.direction = 1 if self.direction == 4 else self.direction+1
-    
-    def fire(self):
-        print("FIRE!")
-        print(self.rect.x)
-        print(self.rect.y)
-        bullet = Bullet(self.rect.x, self.rect.y, self.direction, self.walls)
-        all_sprite_list.add(bullet)
-
-    def changespeed(self, direction):
-        """ Change the speed of the player. """
-        self.direction = direction
- 
-    def update(self):
-        """ Update the player position. """
-        # Move left/right
-        change_x = 0
-        change_y = 0
-        if (self.direction == 1):
-            change_x = 3
-        elif (self.direction == 2):
-            change_y = 3
-        elif (self.direction == 3):
-            change_x = -3
-        elif (self.direction == 4):
-            change_y = -3
-        
-        self.rect.x += change_x
- 
-        # Did this update cause us to hit a wall?
-        block_hit_list = pg.sprite.spritecollide(self, self.walls, False)
-        for block in block_hit_list:
-            # If we are moving right, set our right side to the left side of
-            # the item we hit
-            if change_x > 0:
-                self.rect.right = block.rect.left
-            else:
-                # Otherwise if we are moving left, do the opposite.
-                self.rect.left = block.rect.right
- 
-        # Move up/down
-        self.rect.y += change_y
- 
-        # Check and see if we hit anything
-        block_hit_list = pg.sprite.spritecollide(self, self.walls, False)
-        for block in block_hit_list:
- 
-            # Reset our position based on the top/bottom of the object.
-            if change_y > 0:
-                self.rect.bottom = block.rect.top
-            else:
-                self.rect.top = block.rect.bottom
- 
- 
-class Wall(pg.sprite.Sprite):
-    """ Wall the player can run into. """
-    def __init__(self, x, y, width, height):
-        """ Constructor for the wall that the player can run into. """
-        # Call the parent's constructor
-        super().__init__()
- 
-        # Make a blue wall, of the size specified in the parameters
-        self.image = pg.Surface([width, height])
-        self.image.fill(ORANGE)
- 
-        # Make our top-left corner the passed-in location.
-        self.rect = self.image.get_rect()
-        self.rect.y = y
-        self.rect.x = x
-
-
-class Bullet(pg.sprite.Sprite):
-    """ Wall the player can run into. """
-    def __init__(self, x, y, direction, wall_list):
-        """ Constructor for the wall that the player can run into. """
-        # Call the parent's constructor
-        super().__init__()
- 
-        self.image = bullet
-        self.image = pg.transform.scale(self.image, (10, 10))
-        self.walls = wall_list
- 
-        # Make our top-left corner the passed-in location.
-        self.direction = direction
-        self.rect = self.image.get_rect()
-        if (direction == 2):
-            self.rect.y = y + 100
-            self.rect.x = x + 15
-        elif (direction == 1):#done
-            self.rect.y = y + 22
-            self.rect.x = x + 100
-        elif (direction == 3):#done
-            self.rect.y = y + 20
-            self.rect.x = x - 20
-        elif (direction == 4):#done
-            self.rect.y = y - 10
-            self.rect.x = x + 20
-
-    def update(self):
-        """ Update the player position. """
-        # Move left/right
-        change_x = 0
-        change_y = 0
-        if (self.direction == 1):
-            change_x = 5
-        elif (self.direction == 2):
-            change_y = 5
-        elif (self.direction == 3):
-            change_x = -5
-        elif (self.direction == 4):
-            change_y = -5
-
-        if (self.rect.x > 800 or self.rect.x < 0 or self.rect.y > 600 or self.rect.y < 0):
-            self.kill()
-        
-        self.rect.x += change_x
-        self.rect.y += change_y
-        block_hit_list = pg.sprite.spritecollide(self, self.walls, False)
-        if (len(block_hit_list) >= 1):
-            self.kill()
 
 class GamePlay:
     def __init__(self, game):
@@ -183,12 +49,8 @@ class GamePlay:
             wall = Wall(200, 300, 400, 10)
             wall_list.add(wall)
             all_sprite_list.add(wall)
-        # elif (maze_number == 2):#slanted cross maze
 
-        # elif (maze_number == 3):#spiral
-
-        # Create the player paddle object
-        player = Player(50, 50)
+        player = Player(50, 50, all_sprite_list)
         player.walls = wall_list
          
         all_sprite_list.add(player)
@@ -216,3 +78,4 @@ class GamePlay:
             clock.tick(60)
          
         pg.quit()
+
